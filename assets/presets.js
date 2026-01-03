998b87cb49d15a546d40bb2ecaa95d9af57a5259
daisy920331 <daisy920331@gmail.com>
2026-01-03 09:13:39 +0800

    Create presets.js

---
+++ assets/presets.js
@@ -0,0 +1,274 @@
+// assets/presets.js
+// 來源：你提供的 名單.txt（已整理為「團體/個人」候選池）
+
+export const PRESET_GROUPS = [
+  // 團體（EXILE TRIBE）
+  "EXILE",
+  "EXILE THE SECOND",
+  "三代目J SOUL BROTHERS from EXILE TRIBE (簡稱 3JSB)",
+  "GENERATIONS from EXILE TRIBE (簡稱 GENE)",
+  "THE RAMPAGE from EXILE TRIBE (簡稱 RMPG)",
+  "FANTASTICS from EXILE TRIBE (簡稱 FANTA)",
+  "BALLISTIK BOYZ from EXILE TRIBE (簡稱 BBZ)",
+  "PSYCHIC FEVER from EXILE TRIBE (簡稱 PCF)",
+  "LIL LEAGUE from EXILE TRIBE (簡稱 LIL)",
+  "KID PHENOMENON from EXILE TRIBE (簡稱 KDP)",
+  "THE JET BOY BANGERZ from EXILE TRIBE (簡稱 TJBB)",
+  "WOLF HOWL HARMONY from EXILE TRIBE (簡稱 WHH)",
+  "MA55IVE THE RAMPAGE",
+
+  // 團體（其他）
+  "DEEP",
+  "DEEP SQUAD",
+  "DOBERMAN INFINITY",
+  "PKCZ®",
+  "m-flo",
+  "HONEST BOYZ®",
+  "Girls²",
+  "iScream",
+  "Laki",
+  "f5ve",
+  "RAG POUND",
+  "SWEET REVENGE",
+  "EXILE B HAPPY",
+  "LDH SCREAM",
+  "CIRRA",
+  "劇團EXILE",
+];
+
+// 個人：保留 (兼任...) / <漢字> 的資訊
+export const PRESET_PEOPLE = [
+  // 個人歌手區
+  "Dream Ami",
+  "Dream Shizuka",
+  "VERBAL",
+  "MIYAVI",
+  "Crystal Kay",
+  "JAY'ED",
+  "Leola",
+  "青柳翔",
+  "CRAZY 四角形<八木將康>",
+
+  // 演員區
+  "松本利夫",
+  "RIKACO",
+  "鈴木伸之",
+  "石井杏奈",
+  "山口乃乃華",
+  "早乙女太一",
+  "勝矢",
+  "中山Hinano",
+  "橫山涼",
+
+  // TALENT
+  "山口五和",
+  "LISA",
+
+  // 模特
+  "佐田真由美",
+  "岩堀SERI",
+  "大屋夏南",
+  "杉ARISA",
+  "Taylor鈴木",
+  "佐藤晴美",
+  "藤井夏戀",
+  "MORGAN茉愛羅",
+  "與田EMILY",
+  "東子(TOKO)",
+  "愛芭",
+  "稻田彩羽",
+
+  // 【ARTIST】EXILE
+  "HIRO",
+  "MATSU (兼任 演員)",
+  "ÜSA",
+  "MAKIDAI",
+  "ATSUSHI",
+  "AKIRA",
+  "TAKAHIRO",
+  "橘KENCHI",
+  "TETSUYA",
+  "NESMITH",
+  "SHOKICHI",
+  "NAOTO",
+  "NAOKI<小林直己>",
+  "ALAN<白濱亞嵐>",
+  "SEKAI<世界>",
+  "TAIKI<佐藤大樹>",
+
+  // 三代目
+  "ELLY",
+  "KENJIRO<山下健二郎>",
+  "GAN<岩田剛典>",
+  "RYUJI<今市隆二>",
+  "ØMI",
+
+  // GENERATIONS
+  "RYOTA<片寄涼太>",
+  "RYUTO<數原龍友>",
+  "HAYATO<小森隼>",
+  "REO<佐野玲於>",
+  "YUTA<中務裕太>",
+
+  // THE RAMPAGE
+  "LIKIYA",
+  "ZIN<陣>",
+  "RIKU",
+  "KENTA<神谷健太>",
+  "RUI<与那嶺瑠唯>",
+  "Y.SHOGO<山本彰吾>",
+  "KAZUMA<川村壱馬>",
+  "HOKUTO<吉野北人>",
+  "I.SHOGO<岩谷翔吾>",
+  "SHOHEI<浦川翔平>",
+  "ITSUKI<藤原樹>",
+  "KAISEI<武知海青>",
+  "MAKOTO<長谷川慎>",
+  "RYU<龍>",
+  "TAKAHIDE<鈴木昂秀>",
+  "TAKUMA<後藤拓磨>",
+
+  // FANTASTICS
+  "S.NATSUKI<澤本夏輝>",
+  "LEIYA<瀨口黎彌>",
+  "YUSEI<八木勇征>",
+  "H.NATSUKI<堀夏喜>",
+  "KEITO<木村慧人>",
+  "SOTA<中島颯太>",
+  "SHOTA<中尾翔太>",
+
+  // BALLISTIK BOYZ
+  "RYUTA<日高竜太>",
+  "YOSHIYUKI<加納嘉將>",
+  "RYUSEI<海沼流星>",
+  "MIKU<深堀未來>",
+  "RIKIYA<奧田力也>",
+  "RIKI<松井利樹>",
+  "MASA<砂田將宏>",
+
+  // PSYCHIC FEVER
+  "TSURUGI<剣>",
+  "RYOGA<中西椋雅>",
+  "REN<渡邊廉>",
+  "JIMMY",
+  "KOKORO<小波津志>",
+  "RYUSHIN<半田龍臣>",
+  "WEESA",
+
+  // LIL LEAGUE
+  "SENA<岩城星那>",
+  "TATSU<中村竜大>",
+  "KODAI<山田晃大>",
+  "MATORA<岡尾真虎>",
+  "HAIMA<百田隼麻>",
+  "SORA<難波碧空>",
+
+  // KID PHENOMENON
+  "KENSUKE<夫松健介>",
+  "TSUBASA<遠藤翼空>",
+  "KOHAKUA<岡尾琥珀>",
+  "SOMA<川口蒼真>",
+  "SHUN<佐藤峻乃介>",
+  "KOTA<山本光汰>",
+  "RUI<鈴木瑠偉>",
+
+  // TJBB
+  "YUHI",
+  "AERON",
+  "KOTA",
+  "SHOW",
+  "TAKUMI",
+  "HINATA",
+  "TAKI",
+  "NOSUKE",
+  "SHIGETORA",
+  "AOI",
+
+  // WHH
+  "RYOJI",
+  "SUZUKI",
+  "GHEE",
+  "HIROTO",
+
+  // 其他團體成員（檔案裡有列）
+  "DEEP TAKA",
+  "DEEP YUCHIRO",
+  "DEEP KEISEI",
+
+  "KUBO-C",
+  "GS",
+  "P-CHO",
+  "SWAY",
+  "KAZUKI",
+
+  "DJ DARUMA",
+  "TAKU TAKAHASHI",
+
+  "小田柚葉",
+  "隅谷百花",
+  "鶴屋美咲",
+  "小川桜花",
+  "增田來亞",
+  "菱田未渚美",
+  "山口綺羅",
+
+  "RUI",
+  "YUNA",
+  "HINATA",
+
+  "山口莉愛",
+  "永山椿",
+  "深澤日彩",
+  "比嘉優和",
+  "佐藤栞奈",
+  "上村梨々香",
+  "森朱里",
+  "佐藤妃希",
+
+  "KAEDE",
+  "SAYAKA",
+  "MIYUU",
+  "RURI",
+
+  "Twiggz JUN",
+  "Soulija Twiggz SHOOT",
+  "Jr Twiggz KTR",
+  "Baby Twiggz SHUN",
+  "Kid Twiggz TOMO",
+  "General Twiggz KC",
+  "iLL Twiggz KISHIN",
+  "Prince Twiggz DAISUKE",
+
+  "AGNES",
+  "ASAMI",
+  "LARISSA",
+
+  "航生",
+  "來夢",
+  "RYU-SEI",
+  "笑大郎",
+  "京太郎",
+  "LION",
+  "Ryunosuke",
+  "SHUNNOSUKE",
+  "武藏",
+  "空",
+
+  "MYU.S",
+  "TOA",
+  "YURARA",
+  "YUNA",
+  "MOMO",
+  "NIKORI",
+  "MYU.Y",
+  "KOHARU",
+  "MANON",
+
+  "小澤雄太",
+  "町田啓太",
+  "小野塚勇人",
+  "佐藤寛太",
+  "塩野瑛久",
+  "前田拳太郎",
+  "櫻井佑樹",
+];
